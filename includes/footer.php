f0638a759452fb50cd7659801bb5811c0e84c5e2
Saroj Chaulagain <142041373+SarojChaulagain0@users.noreply.github.com>
2024-01-14 11:45:51 +1100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h3>About EZY RENTALS</h3>
+          <ul>
+            <li><a href="#">Our Story</a></li>
+            <li><a href="#">Leadership</a></li>
+            <li><a href="#">Careers</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>New Cars</h3>
+          <ul>
+            <li><a href="#">Car Listings</a></li>
+            <li><a href="#">Compare Cars</a></li>
+            <li><a href="#">Dealership Locations</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>General Help</h3>
+          <ul>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Financing Options</a></li>
+            <li><a href="#">Warranty</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>Contact Us</h3>
+          <ul>
+            <li><a href="#">Phone: 1234567889</a></li>
+            <li><a href="#">Email: ezyrentals@gmail.com</a></li>
+            <li><a href="#">Address: Sydney, Australia</a></li>
+          </ul>
+          <div class="social-media">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<nav>
+    <div class="logo">
+      <h1>EZY RENTALS</h1>
+    
+    </div>
+    <ul class="nav-links">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="cars.php">Cars</a></li>
+      <li><a href="contact.php">Contact Us</a></li>
+    </ul>
+    <div class="auth-links">
+      <a href="login.php">Login</a>
+      <a href="register.php">Register</a>
+    </div>
+  </nav>
